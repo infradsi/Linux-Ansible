fc7ed07bb04a6820ae4106139bee38c0f3f9e8a8
infradsi <infradsi@gmail.com>
2025-09-02 14:50:10 +0200

    upd

--- Validated/Ansible/check_ssh_access.sh
+++ Validated/Ansible/check_ssh_access.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+touch ./ssh_access_failed.log
+touch ./ssh_access_success.log
+
 #=== CONFIGURATION ========================================================
 HOSTS_FILE="servers.txt"           # Fichier contenant la liste des hôtes (1 par ligne)
 USERNAME="fr-726-ansible"          # Compte à tester
@@ -26,10 +30,13 @@ echo "==================================================================="
 echo
 
 # Boucle sur chaque hôte
-while IFS= read -r host; do
-    [[ -z "$host" || "$host" == \#* ]] && continue
+while IFS= read -r host || [[ -n "$host" ]]; do
+    [[ -z "$host" || "$host" == \#* ]] && continue  # ignorer lignes vides ou commentées - qui commencent par # ou *
+
     echo "Test SSH sur $host ..."
+
     sshpass -p "$PASSWORD" ssh -o ConnectTimeout=$TIMEOUT "$USERNAME@$host" "echo OK" >/dev/null 2>&1
+
     if [[ $? -eq 0 ]]; then
         echo "[✅ SUCCESS] $host"
         echo "$host" >> ssh_access_success.log
@@ -37,11 +44,11 @@ while IFS= read -r host; do
         echo "[❌ FAILURE] $host"
         echo "$host" >> ssh_access_failed.log
     fi
+
     echo
 done < "$HOSTS_FILE"
 
-
 # Résumé
 echo "Résumé :"
-echo "Succès : $(wc -l < ssh_access_success.log 2>/dev/null || echo 0)"
-echo "Échecs : $(wc -l < ssh_access_failed.log 2>/dev/null || echo 0)"
+#echo "Succès : $(wc -l < ssh_access_success.log 2>/dev/null || echo 0)"
+#echo "Échecs : $(wc -l < ssh_access_failed.log 2>/dev/null || echo 0)"
